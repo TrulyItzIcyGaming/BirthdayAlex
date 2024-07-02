7af7592531bfe81172c315d731ca95796edc8ebc
ItzIcyGaming <73133185+TrulyItzIcyGaming@users.noreply.github.com>
2024-07-02 03:31:49 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-const birthday = new Date('2009-07-02');
-const today = new Date();
-const age = today.getFullYear() - birthday.getFullYear();
-const isBirthdayPassed = today.getMonth() > birthday.getMonth() || (today.getMonth() === birthday.getMonth() && today.getDate() >= birthday.getDate());
-const ageCountdown = isBirthdayPassed ? age : age - 1;
-
-document.getElementById('age').textContent = ageCountdown;
